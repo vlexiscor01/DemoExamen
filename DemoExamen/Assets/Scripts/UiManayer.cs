cbf5eff429a71a644cfe46f428bbe35d7e4a887d
vlexiscor01 <vintagecor@outlook.com>
2022-10-24 15:22:38 -0500

    Add Ui element
    Create Ui Element and script start

--- DemoExamen/Assets/Scripts/UiManayer.cs
+++ DemoExamen/Assets/Scripts/UiManayer.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UiManayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void StarGame()
     {
-        
+        SceneManager.LoadScene(1);
+           
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
